2fd6e3ca61ed5bd2f1ad83bd9b22c41171079b4c
David Ghedini <david@davidghedini.com>
2022-07-26 16:45:05 +0300

    Update drop-down.js

--- drop-down.js
+++ drop-down.js
@@ -1,4 +1,10 @@
-$('.select2').each(function() {
+// The last element of the default view.js looks like below:
+// .hide()}()}));  
+// past the code below in the 'PASTE HERE' section shown below:
+// .hide()}() PASTE HERE })); 
+
+.hide()}()
+	$('.select2').each(function() {
 		let obj = $(this);
 		obj.select2({
 			placeholder: obj.attr('placeholder'),
